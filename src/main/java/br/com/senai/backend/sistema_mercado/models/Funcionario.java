41b69948dcf6a9fe327c5860ace6f20e11716434
parmiigianoreggiano <puellamagiaila@gmail.com>
2025-11-13 14:59:50 -0300

    funcionaro get,set, consturct

--- src/main/java/br/com/senai/backend/sistema_mercado/models/Funcionario.java
+++ src/main/java/br/com/senai/backend/sistema_mercado/models/Funcionario.java
@@ -1,12 +1,16 @@
 package br.com.senai.backend.sistema_mercado.models;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,5 +29,55 @@ public class Funcionario {
     private String cargo;
     @Column(name="data_nascimento")
     private LocalDate dataNascimento;
-
+    @ManyToMany
+    @JoinTable( name="funcionario_mercadoria",
+        joinColumns = @JoinColumn(name="id_funcionario", referencedColumnName =  "funcId"),
+        inverseJoinColumns = @JoinColumn(name="id_mercadoria", referencedColumnName = "mercaId")
+    )
+    private List<Mercadoria> mercadorias;
+    public Funcionario(Integer funcId, String nome, String cpf, String cargo, LocalDate dataNascimento,
+            List<Mercadoria> mercadorias) {
+        this.funcId = funcId;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.cargo = cargo;
+        this.dataNascimento = dataNascimento;
+        this.mercadorias = mercadorias;
+    }
+    public Integer getFuncId() {
+        return funcId;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public String getCargo() {
+        return cargo;
+    }
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+    public List<Mercadoria> getMercadorias() {
+        return mercadorias;
+    }
+    public void setFuncId(Integer funcId) {
+        this.funcId = funcId;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    public void setMercadorias(List<Mercadoria> mercadorias) {
+        this.mercadorias = mercadorias;
+    }
 }
